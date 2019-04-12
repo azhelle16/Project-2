7c12b9c406771ce0e9125bcadbd57ab9d569397e
mllsumulong <mllsumulong@gmail.com>
2019-04-11 22:52:01 -0700

    updated server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: '',
   database: 'quiz_app'
 });
 
