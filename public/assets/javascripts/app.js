78e31012be0d7a6d5f564d43adbebbc02e8d19dd
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-04-11 18:34:05 -0700

    fixed app.js and added css

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -57,23 +57,21 @@ function getLevels() {
     for (var levelIndex in l) {
 
       //made var levelNum for creating unique id
-      levelNum = "level" + levelIndex;
+      levelNum = l[levelIndex].id + "level" + levelIndex
 
       levelPara = $("<p>");
 
-      levelPara.html(`<strong>${l[levelIndex].id}. ${l[levelIndex].level_name}</strong> <br>`)
+      levelPara.html(`<strong>${l[levelIndex].level_name}</strong> <br>`)
         .attr("class", "levels")
-        .attr("id", l[levelIndex].id + levelNum);
-
-      // getCategories();
+        .attr("id", levelNum);
 
       $("#topicsDiv").append(levelPara);
+		getCategories(levelNum);
     }
-    getCategories();
   });
 }
 
-function getCategories() {
+function getCategories(lid) {
 
   $.ajax({
     url: "/categories",
@@ -82,50 +80,26 @@ function getCategories() {
 
     for (var i = 0; i < c.length; i++) {
 
-      var catNum = "cat" + i;
+			var catNum = "cat-"+c[i].id
 
-      var catLabel = $("<label>");
-      var catRadio = $("<input>");
+			catLabel = $("<label>");
+			catRadio = $("<input>");
 
-      catLabel.attr("for", catNum)
-        .attr("class", "catLabel")
-        .html(c[i].category_name);
+			catLabel.attr("for", catNum)
+			  .attr("class", "catLabel")
+			  .html(c[i].category_name);
 
-      catRadio.attr("type", "radio")
-        .attr("name", "category")
-        .attr("id", catNum) //not unique id. have to work on this
-        .attr("class", "cat")
-        .attr("value", c[i].id);
+			catRadio.attr("type", "radio")
+			  .attr("name", "category")
+			  .attr("id", catNum) //not unique id. have to work on this
+			  .attr("class", "cat")
+			  .attr("value", c[i].id);
 
-      $("p").eq(0).append(catRadio[0]);
-      $("p").eq(0).append(catLabel[0]);
-      $("p").eq(0).append(catRadio[1]);
-      $("p").eq(0).append(catLabel[1]);
-
-      // levelPara.append(catRadio);
-      // levelPara.append(catLabel);
-
-      
-       catLabel = $("<label>");
-       catRadio = $("<input>");
-
-      catLabel.attr("for", catNum)
-        .attr("class", "catLabel")
-        .html(c[i].category_name);
-
-      catRadio.attr("type", "radio")
-        .attr("name", "category")
-        .attr("id", catNum) //not unique id. have to work on this
-        .attr("class", "cat")
-        .attr("value", c[i].id);
-
-      $("p").eq(1).append(catRadio[0]);
-      $("p").eq(1).append(catLabel[0]);
-      $("p").eq(1).append(catRadio[1]);
-      $("p").eq(1).append(catLabel[1]);
-
-      // levelPara.append(catRadio);
-      // levelPara.append(catLabel);
+			$("#"+lid).append(catRadio);
+			$("#"+lid).append(catLabel);
+		
     }
+
   });
+
 }
