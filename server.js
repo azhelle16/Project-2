535d1185055a45e12180237f48d552f8c0eb6521
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-04-14 19:48:42 -0700

    added user_score in users table and made other changes accordingly

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/answers', function(req, res) {
 
 app.post('/sign-up', function(req, res) {
   var pass = md5(req.body.password)
-  connection.query('INSERT INTO users (username, password, role, team_id) VALUES (?,?,?,?)', [req.body.name, pass, 0, req.body.team],  function(error, results, fields) {
+  connection.query('INSERT INTO users (username, password, role, team_id, user_score) VALUES (?,?,?,?,?)', [req.body.name, pass, 0, req.body.team, 0],  function(error, results, fields) {
     if (error) res.send({error : error})
     // else res.json({id : results.insertId});
     else res.json({id : results.insertId.toString()});
@@ -107,33 +107,14 @@ app.post('/login', function(req, res) {
   });
 });
 
-app.post('/scores-insert/:user_id', function(req, res){
-  connection.query('INSERT INTO scores (user_id, score) VALUES (?,?)', 
-  [req.params.user_id, req.body.score],function (error, results, fields) {
-    if (error) res.send(error)
-    else res.json({message: 'data added in scores table'});
-  });
-});
-
-//this query might not be needed as there will be only one row per user
-//total user score will be calculated in app.js
-app.get('/scores-total/:user_id', function(req, res) {
-  connection.query('SELECT SUM(score) AS Total_Score FROM scores WHERE user_id = ?',[req.params.user_id],function(error, results, fields) {
-    if (error) res.send(error)
-    else res.send(results[0]);
-  });
-});
-
 //gets all users and their ranks with same rank for tied scores
 app.get('/all-user-ranks', function(req, res) {
 
-  const allRanks = `SELECT users.id, users.username, scores.score,
+  const allRanks = `SELECT id, username, user_score,
                     DENSE_RANK() OVER (
-                      ORDER BY score DESC
+                      ORDER BY user_score DESC
                     ) user_rank
-                    FROM scores
-                    LEFT JOIN users
-                    ON scores.user_id = users.id`
+                    FROM users`
 
   connection.query(allRanks,function(error, results, fields) {
     if (error) res.send(error)
@@ -146,13 +127,11 @@ app.get('/user-rank/:user_id',function(req, res) {
 
   const userRank = `SELECT id,name,score,user_rank 
                     FROM (
-                    SELECT users.id id, users.username name , scores.score score,
+                    SELECT id, username name , user_score score,
                     DENSE_RANK() OVER (
-                    ORDER BY score DESC
+                    ORDER BY user_score DESC
                     ) user_rank
-                    FROM scores
-                    LEFT JOIN users
-                    ON scores.user_id = users.id
+                    FROM users
                     ) rankingTable 
                     WHERE id = ?`
 
@@ -166,18 +145,16 @@ app.get('/user-rank/:user_id',function(req, res) {
 app.get('/team-ranks', function(req, res) {
 
   const teamRanks =`SELECT DENSE_RANK() OVER (
-                    ORDER BY SUM(scores.score) DESC
+                    ORDER BY SUM(user_score) DESC
                     ) Team_Rank,
                     teams.team_name AS Team_Name, 
                     teams.id AS Team_ID, 
-                    SUM(scores.score) AS Team_Score
-                    FROM scores
-                    LEFT JOIN users
-                    ON scores.user_id = users.id
+                    SUM(user_score) AS Team_Score
+                    FROM users
                     LEFT JOIN teams
                     ON users.team_id = teams.id
                     GROUP BY users.team_id
-                    ORDER BY SUM(scores.score) DESC`
+                    ORDER BY SUM(user_score) DESC`
 
   connection.query(teamRanks,function(error, results, fields) {
     if (error) res.send(error)
@@ -190,14 +167,12 @@ app.get('/team-score', function(req, res) {
 
   const q =`SELECT users.team_id AS Team_Id, 
             teams.team_name AS Team_Name, 
-            SUM(scores.score) AS Team_Score
-            FROM scores
-            LEFT JOIN users
-            ON scores.user_id = users.id
+            SUM(user_score) AS Team_Score
+            FROM users
             LEFT JOIN teams
             ON users.team_id = teams.id
             GROUP BY users.team_id
-            ORDER BY SUM(scores.score) DESC`;
+            ORDER BY SUM(user_score) DESC`;
 
   connection.query(q,function(error, results, fields) {
     if (error) res.send(error)
