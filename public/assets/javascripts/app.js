2e88a83e44a388e055ce6d5e8bbea60eb394850b
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-04-17 21:54:44 -0700

    team rank,no. of players,styling

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -549,6 +549,41 @@ function getCurrUserRank(id){
 /*
  #######################################################################
  #
+ #  FUNCTION NAME : currUserTeamRank
+ #  AUTHOR        : Juthika Shetye
+ #  DATE          : April 17, 2019 PDT
+ #  MODIFIED BY   : 
+ #  REVISION DATE : 
+ #  REVISION #    : 
+ #  DESCRIPTION   : returns user's id and team's rank
+ #  PARAMETERS    : 
+ #
+ #######################################################################
+*/
+
+function currUserTeamRank(id){
+
+  var trank;
+
+  $.ajax({
+    url: '/team-rank/' + id,
+    method: 'GET',
+    async: false,
+  }).done(function(tr){
+
+    //returns user's id and team's rank
+    console.log(tr);
+    
+    trank = tr;
+
+  });
+
+  return trank;
+}
+
+/*
+ #######################################################################
+ #
  #  FUNCTION NAME : updateUserScore
  #  AUTHOR        : Juthika Shetye
  #  DATE          : April 16, 2019 PDT
@@ -587,9 +622,9 @@ function updateUserScore(id,callback){
  #  AUTHOR        : Juthika Shetye
  #  DATE          : April 13, 2019 PDT
  #  MODIFIED BY   : 
- #  REVISION DATE : 
- #  REVISION #    : 
- #  DESCRIPTION   : returns all teams' ranks
+ #  REVISION DATE : April 17, 2019 PDT
+ #  REVISION #    : 1
+ #  DESCRIPTION   : returns all teams' ranks and no. of players per team
  #  PARAMETERS    : 
  #
  #######################################################################
@@ -600,11 +635,12 @@ function teamRanks(){
   var table = $('<table>').addClass('table table-striped table-bordered');      
   var thead = $('<thead>').addClass('thead-dark');  
   var tr = $('<tr>');        
-  var thTeam = $('<th>').text('Team Name');  
+  var thTeam = $('<th>').text('Team Name');
+  var thPlayers = $('<th>').text('Players');
   var thScore = $('<th>').text('Score');
   var thRank = $('<th>').text('Rank');
 
-  tr.append(thTeam, thScore, thRank);
+  tr.append(thTeam, thPlayers, thScore, thRank);
   thead.append(tr);             
   table.append(thead);
 
@@ -621,10 +657,11 @@ function teamRanks(){
 
       var tr = $('<tr>');
       var tdTeam = $('<td>').text(t[i].Team_Name);
+      var tdPlayers = $('<td>').text(t[i].No_Of_Players);
       var tdScore = $('<td>').text(t[i].Team_Score);
       var tdRank = $('<td>').text(t[i].Team_Rank);
 
-      tr.append(tdTeam, tdScore, tdRank);
+      tr.append(tdTeam, tdPlayers, tdScore, tdRank);
       tbody.append(tr);
       table.append(tbody);
       $('#teamRanksDiv').append(table);
@@ -664,6 +701,8 @@ function testingFunctions() {
     // getCurrUserRank(globalUserId);
 
     teamRanks();
+
+    currUserTeamRank(globalUserId);
 }
 
 
